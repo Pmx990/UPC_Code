76f84bca6c457ea27a3e570c1d657789aeec601c
Junzhe Liang <junzhe.liang@bellevuecollege.edu>
2018-11-19 16:41:17 -0800

    Update BarcodeBSTScanner.cpp

--- BarcodeBSTScanner.cpp
+++ BarcodeBSTScanner.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include <cassert>
+#include <string>
 #include "BinarySearchTree.h"
 
 using namespace std;
 
 int main(){
 			BinarySearchTree<string> tree;
+			ifstream readFile("upc_corpus.txt");
+			string temp;
+			while(getline(readFile,temp)){
+				tree.insert(temp);
+			}
 			return 0;
 }
