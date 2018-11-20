041a26b4aeb48de4fc8b74e8ab03e8ca22726178
Junzhe Liang <junzhe.liang@bellevuecollege.edu>
2018-11-19 17:32:42 -0800

    Update BarcodeBSTScanner.cpp

--- BarcodeBSTScanner.cpp
+++ BarcodeBSTScanner.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <cassert>
 #include <string>
-#include "BinarySearchTree.h"
+#include "DataType.h"
 
 using namespace std;
 
 int main(){
-			BinarySearchTree<string> tree;
+			BinarySearchTree<upc> tree;
 			ifstream readFile("upc_corpus.txt");
-			string temp;
+			string blank = "n";
+			string temp2;
+			string key;
+			string value;
 			int a = 0;
-			while(getline(readFile,temp)){
-				tree.insert(temp);
-				a++;
-			}
-							cout<<a<<endl;
+			while(getline(readFile,temp2)){
+				istringstream ss(temp2);
+				getline(ss,key,',');
+				getline(ss,value,',');
+				upc temp = {key,value};
 
+				tree.insert(temp);
+			}
 			readFile.close();
 			return 0;
 }
+
+
+
